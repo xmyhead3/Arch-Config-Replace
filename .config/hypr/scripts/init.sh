b7d27da38f622e21654af5ace43921252618a2ef
eprahemi <eprahemi@users.noreply.github.com>
2026-04-05 19:55:53 +0200

    fix matugen not reloading apps on startup due to 4.0 compatability issues

--- .config/hypr/scripts/init.sh
+++ .config/hypr/scripts/init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 FLAG="$HOME/.cache/wallpaper_initialized"
+RELOAD_SCRIPT_PATH="$HOME/.config/hypr/scripts/quickshell/wallpaper/matugen_reload.sh"
 
 [ -f "$FLAG" ] && exit 0
 
@@ -10,8 +11,14 @@ file=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -in
 
 if [ -n "$file" ]; then
     cp "$file" /tmp/lock_bg.png
+    
     awww img "$file" --transition-type any --transition-pos 0.5,0.5 --transition-fps 144 --transition-duration 1 &
-    matugen image "$file" --source-color-index 0 
+    
+    if matugen image "$file" --source-color-index 0; then
+        if [ -f "$RELOAD_SCRIPT_PATH" ]; then
+            bash "$RELOAD_SCRIPT_PATH"
+        fi
+    fi
 fi
 
 mkdir -p "$(dirname "$FLAG")"
